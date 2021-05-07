fd345bd403d16efc21def81305e8191ba5b4231d
LSailer <sailer.luca@gmx.de>
2021-05-07 15:57:36 +0200

    DB Colletcion Funktion User Login implementiert

--- CeenitBackend/main.py
+++ CeenitBackend/main.py
@@ -1,14 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
 from fastapi import FastAPI, File, UploadFile, status
-from fastapi.responses import FileResponse
 import logging
-import json
-from time import sleep
-from datetime import datetime
-from threading import Thread
-from xml.dom import minidom
-import yaml
+import uvicorn
+import BaseModel.ModelUser
 
 # globale Varibalen
+import dbConnection
+
 VERSION = '1.0'
 app = FastAPI()
 
@@ -16,59 +19,66 @@ app = FastAPI()
 logging.basicConfig(format='%(asctime)s: %(message)s',
                     level=logging.INFO, datefmt='%H:%M:%S')
 
-#Logging einlesen
-with open('logging.yaml', 'r') as f:
-    config_log = yaml.safe_load(f.read())
-    logging.config.dictConfig(config_log)
 
-@app.get("/versionsinfo",  status_code=status.HTTP_200_OK)
+@app.get("/versionsinfo")
 def version():
     return {'version': VERSION}
 
 
-@app.post("/CreateUser", status_code=status.HTTP_200_OK)
-def createUser( file: UploadFile = File(...)):
+@app.post("/CreateUser")
+def createUser(user: BaseModel.ModelUser.User):
     pass
 
-@app.post("/LoginUser", status_code=status.HTTP_200_OK)
-def getSession(file: UploadFile = File(...)):
-    pass
+
+@app.post("/LoginUser")
+def getLogin(user: BaseModel.ModelUser.User):
+    return dbConnection.loginUser(user.username, user.password)
 
 @app.get("/movie/getFiveHottest")
 def getFiveHottest():
     pass
 
+
 @app.get("/movie/search/{title}")
 def getMovieSearch(title: str):
     pass
 
+
 @app.post("/movie/review/{movieid}")
 def postReviewToMovie(movieid: int):
     pass
+
 
 # localhost:1234/account/watchlist?userid=1221234
 @app.get("/account/watchlist/{userid}")
 def getWatchlist(userid: int):
     pass
 
+
 @app.post("/account/watchlist/{userid}")
 def addWatchlist(userid: int):
     pass
+
 
 @app.get("/account/timeline/{userid}")
 def getTimeLine(userid: int):
     pass
 
+
 @app.post("/account/timeline/{userid}")
 def getTimeLine(userid: int):
     pass
 
+
 @app.post("/account/{userid}")
 def addTimeLine(userid: int):
     pass
+
 
 @app.get("/account/reviews/{userid}")
 def getReviews(userid: int):
     pass
 
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=1234)
--- CeenitBackend/unit-Test/db_test.py
+++ CeenitBackend/unit-Test/db_test.py
@@ -1,15 +1,20 @@
 import dbConnection
 import unittest
+from  fastapi import HTTPException
 
 class TestDB(unittest.TestCase):
     def test_connection(self):
         db= dbConnection.DBconnect()
         myCol = db["users"]
         for x in myCol.find():
-            self.assertEqual(str(x), "{'_id': ObjectId('60814dab36edb0a032855243'), 'username': 'Testuser Stark', 'email': 'sean_bean@asd.com', 'fristname': 'Sean', 'lastname': 'Bean', 'password': '$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu'}")
-    def test_getUserIDByUsername(self):
-        id = dbConnection.getUsersIdByUsername("Testuser_Stark")
+            self.assertEqual(str(x), "{'_id': ObjectId('60814dab36edb0a032855243'), 'username': 'TestuserStark', 'email': 'sean_bean@asd.com', 'fristname': 'Sean', 'lastname': 'Bean', 'password': 'testpassword'}")
+
+    def test_loginUser(self):
+        id = dbConnection.loginUser("TestuserStark", "testpassword")
         self.assertEqual(id, "60814dab36edb0a032855243")
-        
+        with self.assertRaises(HTTPException) as context:
+            dbConnection.loginUser("failUser", "failPassword")
+        self.assertTrue('Gültige Authentifizierung' in context.exception.detail)
+
 if __name__ == '__main__':
     unittest.main()
