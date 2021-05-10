b9ed076bbe48a139db51956ab2127d5311e46224
Nono1337 <83017031+Nono1337@users.noreply.github.com>
2021-05-10 11:21:13 +0200

    materials

--- CeenitFrontend/ceenit/src/app/app.module.ts
+++ CeenitFrontend/ceenit/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { CeenitListsComponent } from './ceenit-lists/ceenit-lists.component';
 import { CeenitReviewsComponent } from './ceenit-reviews/ceenit-reviews.component';
 import { CeenitWatchlistComponent } from './ceenit-watchlist/ceenit-watchlist.component';
 import { CeenitTimelineComponent } from './ceenit-timeline/ceenit-timeline.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { CeenitTimelineComponent } from './ceenit-timeline/ceenit-timeline.compo
     CeenitTimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
