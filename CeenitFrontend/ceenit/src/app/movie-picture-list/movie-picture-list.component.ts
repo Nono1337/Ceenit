27d5d79ec57a2b8ab6f2dd6ef8be0baf34f14484
ValentinNeumann <valentin.neumann2001@gmail.com>
2021-05-27 09:18:11 +0200

    Added a Top 5 Hottest List

--- CeenitFrontend/ceenit/src/app/movie-picture-list/movie-picture-list.component.ts
+++ CeenitFrontend/ceenit/src/app/movie-picture-list/movie-picture-list.component.ts
@@ -1,24 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from 'src/app/service/http-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { HottestMovie } from '../Model/HottestMovie.model';
+import { asLiteral } from '@angular/compiler/src/render3/view/util';
 
 @Component({
   selector: 'app-movie-picture-list',
   templateUrl: './movie-picture-list.component.html',
   styleUrls: ['./movie-picture-list.component.css']
 })
+
 export class MoviePictureListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpServe: HttpServiceService, private route: ActivatedRoute) {}
+  hotMovies: HottestMovie[];
 
   ngOnInit(): void {
+    this.hotMovies = new Array<HottestMovie>();
+    this.httpServe
+      .getFiveHottestMovies()
+      .subscribe((resp) => {
+        console.log(resp);
+        if (resp != undefined) {
+          // @ts-ignore: TS2339
+          resp.forEach((movie) => {
+            this.hotMovies.push(movie);
+          });
+        }
+      });
   }
 
-  tiles: Tile[] = [
-    {title: 'Movie Title', picture: 'https://i.pinimg.com/originals/6c/0f/d1/6c0fd186a12815a51ce2e3298f6ab9cd.jpg'},
-    {title: 'Among Us', picture: 'https://i.redd.it/3trl9ojxiop51.png'},
-    {title: 'Godzilla', picture: 'https://cdn20.pamono.com/p/z/7/2/728702_agebtldars/vintage-french-godzilla-film-movie-poster-by-a-poucel-1954-1.jpg'},
-    {title: 'Hacker', picture: 'https://www.arthipo.com/image/cache/catalog/poster/movie/759-1554/pfilm1491-hacker_7cf19a97-poster-movie-film-1000x1000.jpg'},
-    {title: 'Shrek3', picture: 'https://i.pinimg.com/originals/24/ea/a8/24eaa8b4e85cc578238ed903bf2494d6.jpg'},
-  ];
+  show(): void {
+    alert('Info')
+  }
+
+  toggle(event) {
+    console.log(event.target); 
+ }
 }
 
 export interface Tile {
