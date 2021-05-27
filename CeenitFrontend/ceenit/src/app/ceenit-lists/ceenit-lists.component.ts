7a90b4497339b5daef0ccf27d57dc8647e8ca247
ValentinNeumann <valentin.neumann2001@gmail.com>
2021-05-27 14:27:30 +0200

    fixed merge

--- CeenitFrontend/ceenit/src/app/ceenit-lists/ceenit-lists.component.ts
+++ CeenitFrontend/ceenit/src/app/ceenit-lists/ceenit-lists.component.ts
@@ -3,12 +3,8 @@ import { MatSort} from '@angular/material/sort';
 import { MatTableDataSource} from '@angular/material/table';
 import { HttpServiceService } from 'src/app/service/http-service.service';
 import { ActivatedRoute } from '@angular/router';
-<<<<<<< HEAD
 import { Movie } from '../Model/movie.model';
 import { MovieList } from '../Model/MovieList.model'
-=======
-import { MovieList } from '../Model/movie.model';
->>>>>>> 0d251c847d05df4339a5a38b21a3bb9318dfc01d
 
 @Component({
   selector: 'app-ceenit-lists',
