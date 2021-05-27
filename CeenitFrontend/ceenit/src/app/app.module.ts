57f70e9e4ea238fdc585d88de44417fa62542c72
Nono1337 <83017031+Nono1337@users.noreply.github.com>
2021-05-27 15:14:45 +0200

    ListsContent

--- CeenitFrontend/ceenit/src/app/app.module.ts
+++ CeenitFrontend/ceenit/src/app/app.module.ts
@@ -23,12 +23,8 @@ import { MovieListComponent } from './pages/movie-list/movie-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MoviePictureListComponent } from './movie-picture-list/movie-picture-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
-<<<<<<< HEAD
 import { CeenitListsContentComponent } from './ceenit-lists-content/ceenit-lists-content.component'
-=======
 import { MatCardModule } from '@angular/material/card';
-import { FlexLayoutModule } from '@angular/flex-layout';
->>>>>>> 0d251c847d05df4339a5a38b21a3bb9318dfc01d
 
 @NgModule({
   declarations: [
@@ -59,7 +55,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     HttpClientModule,
     MatTableModule,
     MatCardModule,
-    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
